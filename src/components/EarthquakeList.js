b4f6809148c3e9586ed27a71c219b18f3c90a3ac
Jason Tuttle <jason@hearspotbark.com>
2017-07-20 10:45:47 -0400

    hardmode complete

--- src/components/EarthquakeList.js
+++ src/components/EarthquakeList.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import earthquakes from '../data/earthquakes';
 import EarthquakeInfo from './EarthquakeInfo';
+import 'whatwg-fetch';
 import '../styles/App.css';
 
 class EarthquakeList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {quakeData: []};
+  }
+  componentDidMount() {
+    fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson')
+      .then(response => response.json())
+      .then(data => this.setState({quakeData: data.features}));
+  }
   render() {
-    const earthquakeList = earthquakes.features;
+    const {quakeData} = this.state;
+    console.log(quakeData);
     return (
       <div className='quake-list'>
-        {earthquakeList.map((quake, index) =>
+        {quakeData.map((quake, index) =>
           <EarthquakeInfo
             key={index}
             quake={quake}
